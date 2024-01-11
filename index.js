6dc62c3710a45cb57b0ed7cea3665893357fa45f
Clay Mangum <clay@trinitasaccounting.com>
2024-01-11 06:01:08 -0600

    complete lab

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let employee = {
 
 
 function updateEmployeeWithKeyAndValue(obj, key, value) {
-    //: this function should take in three arguments: an employee Object, a key and a value. This function should not mutate the employee; it should return a new Object that has an updated value for the key passed in. Hint: use the spread operator!
     let newObj = { ...obj };
     newObj[key] = value;
     return newObj;
@@ -15,13 +14,11 @@ function updateEmployeeWithKeyAndValue(obj, key, value) {
 
 
 function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
-    //: this function should work the same as updateEmployeeWithKeyAndValue() but it should mutate the employee Object passed in.
     obj[key] = value;
     return obj;
 }
 
 function deleteFromEmployeeByKey(obj, key) {
-    //: this function should take in a employee Object and a key. It should delete the property with that key from the employee Object. This should not mutate the original employee Object; it should return a new Object that doesn't include the identified key-value pair. Hint: use the spread operator!
     let newObj = { ...obj };
     delete newObj[key];
     return newObj;
@@ -29,7 +26,6 @@ function deleteFromEmployeeByKey(obj, key) {
 
 
 function destructivelyDeleteFromEmployeeByKey(obj, key) {
-    //: this function should work the same as deleteFromEmployeeByKey() but it should mutate the employee Object.
     delete obj[key];
     return obj;
 }
